688445ac931bf90aab4cbda86028afa8bb3f6a8c
NurmuhammadvaSaidAmir <clash010a@gmail.com>
2025-04-02 21:26:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let fruits = ['apple', 'banana', 'orange', 'shaftoli'];
+console.log(fruits);
+console.log(fruits.length);
+let fruits2 = ['apple', 'banana', 'orange', 'shaftoli'];
+console.log(fruits2);
+console.log(fruits2.length);
+let last = fruits2.pop();
+alert('Boshlanishda arryining uzunligi 4:')
+alert('sizning arryiyingizdan malumotlarni tashlamoqchimiz')
+alert('arryingi uzunligi: 3')
